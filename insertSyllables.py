6fff511f60638444444a0e936c13941e64153319
Aditya Dan <danaditya98@gmail.com>
2023-12-07 19:40:25 +0530

    symbolMaps.py: all symbols must be registered in this file

--- insertSyllables.py
+++ insertSyllables.py
@@ -1,19 +1,7 @@
 from trie import *
+from symbolMaps import *
 
 trieObject = Trie()
-
-onsetMap = {"k": "ಕ", "kh": "ಖ", "g": "ಗ", "gh": "ಘ", "c": "ಚ", "ch": "ಛ", "j": "ಜ", "jh": "ಝ", "jn": "ಜ್ಞ", "T": "ಟ",
-            "Th": "ಠ", "D": "ಡ", "Dh": "ಢ", "N": "ಣ", "t": "ತ", "th": "ಥ", "d": "ದ", "dh": "ಧ", "n": "ನ", "p": "ಪ",
-            "ph": "ಫ", "b": "ಬ", "bh": "ಭ", "m": "ಮ", "y": "ಯ", "r": "ರ", "l": "ಲ", "v": "ವ", "sh": "ಶ", "S": "ಷ",
-            "s": "ಸ", "h": "ಹ", "L": "ಳ"}
-
-vowelDiacriticMap = {"aa": "ಾ", "i": "ಿ", "ii": "ೀ", "u": "ು", "uu": "ೂ", "R": "ೃ", "e": "ೆ", "ee": "ೇ", "o": "ೊ",
-                     "oo": "ೋ", "ai": "ೈ", "au": "ೌ"}
-
-vowelMap = {"a": "ಅ", "aa": "ಆ", "i": "ಇ", "ii": "ಈ", "u": "ಉ", "uu": "ಊ", "R": "ಋ", "e": "ಎ", "ee": "ಏ", "o": "ಒ",
-            "oo": "ಓ", "ai": "ಐ", "au": "ಔ"}
-
-codaMap = {"M": "ಂ", "H": "ಃ"}
 
 
 def construct_syllable_map(onset_map, vowel_map):
